42334b922b3be2e8fca82759b0af728d0fb167e8
karol-stola <karolstola@gmail.com>
2019-08-11 19:43:44 +0200

    moved opti classes to library

--- Opti200.cpp
+++
@@ -1,7 +0,0 @@
-#include "Opti200.h"
-#include "OptiStepperTMC2208.h"
-
-OptiStepper Opti200::CreateStepper()
-{
-	return OptiStepperTMC2208(GetMotorDriverActivationPin(), GetMotorStepPin(), GetMotorDirectionPin(), & Serial1, 115200);
-}
--- OptiStepper.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTI_STEPPER_H
-#define OPTI_STEPPER_H
-
-class OptiStepper
-{
-	
-};
-
-#endif
